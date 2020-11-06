b752b1133db095579b2b70dfffab28932b008add
g031s305 <g031s305@s.iwate-pu.ac.jp>
2020-11-06 15:52:10 +0900

    third upload

---
+++ 0312020305_npm_learn.js
@@ -0,0 +1,16 @@
+/*
+パッケージ：hotnode
+
+インストール
+$ sudo npm install -g hotnode
+
+実行
+$ hotnode app.js
+
+結果
+hotnode  node process restarted
+hotnode  routes/index.js has been changed
+hotnode  node process restarted
+hotnode  views/index.ejs has been changed
+hotnode  node process restarted
+*/
